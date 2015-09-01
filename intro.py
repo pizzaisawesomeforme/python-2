5224e443238a0137a1546966c13ddc995622905c
Gustavo Cruz <pizzaisawesomeforme@gmail.com>
2015-08-31 20:40:03 -0400

    save

--- intro.py
+++ intro.py
@@ -1,1 +1,6 @@
+"""The Story of Bob
+
+    "Huh... where am I?" "What is this place and who am I?" You are
+Bob, and you have been stranded by your family for your own safty.
+Umm you don't want to know why... anyways lets get to the adventure.
 """
