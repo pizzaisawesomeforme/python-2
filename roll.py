812413b72db4de434339e89329149ca226fce5c3
Gustavo Cruz <pizzaisawesomeforme@gmail.com>
2015-08-31 20:22:40 -0400

    save

--- roll.py
+++ roll.py
@@ -13,3 +13,16 @@ def roll(howmany=1,sides=6):
 if __name__ == '__main__':
     number = roll()
     print(number)
+
+def parse(text):
+    """Parse traditional dice notation (ex: 3d6)"""
+    howmany, sides = text.split('d')
+    return (int(howmany), int(sides))
+
+def parse_roll(text):
+    """Parses traditional dice notation and then rolls (ex: 3d6)"""
+    howmany, sides = parse(text)
+    return roll(howmany, sides)
+
+if __name__ == '__main__':
+    print(parse_roll('2d6'))
